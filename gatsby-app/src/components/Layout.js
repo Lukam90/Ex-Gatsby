0c200ae4fda8c94bd8acaea81d5bbde60fd036ee
Lukam90 <lucien.hamm@gmail.com>
2023-03-07 14:18:50 +0100

    MAJ Ex Styles

--- gatsby-app/src/components/Layout.js
+++ gatsby-app/src/components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Navbar from './Navbar'
 
+import '../styles/global.css'
+
 export default function Layout({ children }) {
     return (
         <div className='layout'>
--- gatsby-app/src/pages/projects.js
+++ gatsby-app/src/pages/projects.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 
+import * as styles from "../styles/projects.module.css"
+
 export default function Projects() {
     return (
         <Layout>
-            <h2>Portfolio</h2>
+            <div className={styles.portfolio}>
+                <h2>Portfolio</h2>
 
-            <h3>Projects & Websites I've Created</h3>
+                <h3>Projects & Websites I've Created</h3>
+            </div>
         </Layout>
     )
 }

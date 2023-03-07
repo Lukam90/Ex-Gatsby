9e6444866b751823096584d28ed55ce64cf644e2
Lukam90 <lucien.hamm@gmail.com>
2023-03-07 13:51:32 +0100

    MAJ Ex Pages

---
+++ gatsby-app/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1>About Page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolore rerum aut, quia minima qui at beatae ab totam unde.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint repellat numquam, obcaecati similique autem rerum. Ea itaque assumenda doloremque hic?</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum repellat veniam ad laborum laboriosam facilis perspiciatis consectetur ab neque aut?</p>
+    </div>
+  )
+}

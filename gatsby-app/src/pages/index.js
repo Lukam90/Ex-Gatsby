5bc9efbb78cf35c7dad13b380975eca63962d5f2
Lukam90 <lucien.hamm@gmail.com>
2023-03-07 13:55:45 +0100

    MAJ Ex Navbar & Links

--- gatsby-app/src/pages/index.js
+++ gatsby-app/src/pages/index.js
@@ -1,15 +1,19 @@
 import * as React from "react"
 
+import Navbar from "../components/Navbar"
+
 export default function Home() {
-  return (
-    <section>
-      <div>
-        <h2>Design</h2>
+	return (
+		<section>
+			<Navbar />
 
-        <h3>Develop & Deploy</h3>
+			<div>
+				<h2>Design</h2>
 
-        <p>UX designer & web developer based in Manchester</p>
-      </div>
-    </section>
-  )
+				<h3>Develop & Deploy</h3>
+
+				<p>UX designer & web developer based in Manchester</p>
+			</div>
+		</section>
+	)
 }
